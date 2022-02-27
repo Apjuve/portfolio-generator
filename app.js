cc856ec3cb16ac434401b8972c0a4e0e26f85e13
Apjuve <abiponce3@hotmail.com>
2022-02-26 22:55:54 -0500

    added questions

--- app.js
+++ app.js
@@ -22,18 +22,34 @@
 // const generatePage = () => 'Name: Jane, Github: janehub';
 // console.log(generatePage());
 
-const fs = require('fs');
 
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+console.log(inquirer);
 
-const pageHTML = generatePage(name, github);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
 
-fs.writeFile('index.html', pageHTML, err => {
-  if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const fs = require('fs');
+
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+
+// fs.writeFile('index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
 
